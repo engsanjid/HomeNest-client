def5887b6b79e1a088d8a424a75f1320d4b16946
engsanjid <mdsanjidi36@gmail.com>
2025-11-09 20:12:26 +0600

    Removed exposed Firebase key and added environment variables

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -1,22 +1,20 @@
 
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"; 
+import { getAnalytics } from "firebase/analytics";
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDntUCdUNTdpdh4vXQZOvW4C-6wJssDtrU",
-  authDomain: "homenest-e6fc4.firebaseapp.com",
-  projectId: "homenest-e6fc4",
-  storageBucket: "homenest-e6fc4.firebasestorage.app",
-  messagingSenderId: "757961937288",
-  appId: "1:757961937288:web:a23868e573c114fa1aaa3d",
-  measurementId: "G-359R90JNDE"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 
 const app = initializeApp(firebaseConfig);
-
-
-export const auth = getAuth(app);
-
+const analytics = getAnalytics(app);
 
 export default app;
