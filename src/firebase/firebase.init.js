f176e80b1a786ff4b960a2cc87f77d679ba1c809
engsanjid <mdsanjidi36@gmail.com>
2025-11-09 20:36:50 +0600

    Remove old .env from tracking and update secure config

--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -1,5 +1,6 @@
 
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth"; 
 import { getAnalytics } from "firebase/analytics";
 
 
@@ -15,6 +16,9 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app); 
+
+
 const analytics = getAnalytics(app);
 
 export default app;
